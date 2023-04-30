18bf13946163db3d46f04243da239c6f5d44bc99
Everett Butler <everettjamesbutler@gmail.com>
2023-04-30 18:37:48 -0500

    got my basic function component set up for search

---
+++ src/components/SearchView/SearchView.jsx
@@ -0,0 +1,13 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios'
+
+function SearchView () {
+
+    return (
+        <>
+        
+        </>
+    );
+}
+
+export default SearchView();
